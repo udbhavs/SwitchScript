2cae1711e4ae2282764a3a9f535d6bb4083249e8
Udbhav <udbhavs123@gmail.com>
2017-06-09 17:43:34 +0530

    Added new files
    Changes: 
    1.) Keywords are replaced through key-value pairs of words object, not manually.
    2.) Added incomplete create.html and respective JS file
    3.) Edited top menu to be static
    
    To Do:
    1.) Make create.html work, possibly through PHP or merging pages to use the same script
    2.) Make replacements of single characters stable
    
    Note- Since replacements are now done using the replace() function, replacing single characters is not preferred, and can be unstable.

--- new SwitchScript/js/home.js
+++ new SwitchScript/js/home.js
@@ -1,3 +1,18 @@
+//needed by other script - default config
+var words = {
+        
+        "var" : "for",
+        "for" : "var",
+        "while" : "function",
+        "function" : "while",
+        "console.log" : "externalConsoleReport"
+        
+}
+//needed by other script
+function setObj(obj) {
+    words = obj;
+}
+
 window.onload = function() {
 
     //css and styling - MAIN HEAD
@@ -6,32 +21,20 @@ window.onload = function() {
     var textarea = document.getElementById("textarea");
     var reports = document.getElementById("reports"); //where every console.log goes
     
-    //menu options
-    var home = document.getElementById("home");
-    var about = document.getElementById("about");
+    //menu
     var run = document.getElementById("run");
+    var home = document.getElementById("home");
     var clear = document.getElementById("clear");
+    var about = document.getElementById("about");
     
-    var menu = [home,about,run,clear];
-    
-    //handlers
-    function mainheadHandler() {
-        for (var i = 0; i < menu.length; i++) {
-            menu[i].classList.add("viewable");
-            menu[i].style.left = ( (window.innerWidth/menu.length) * (i + 0.25) ) + "px";
-        }
+    //menu button functions
+    try {
+        run.addEventListener("click", function() { evaluate(textarea.value); });
+        about.addEventListener("click", function() { window.location.href = "about.html" });
+        home.addEventListener("click", function() { window.location.href = "index.html" });
+        clear.addEventListener("click", function() { reports.innerHTML = "";});
     }
-    function mainheadHandler2() {
-        for (var i = 0; i < menu.length; i++) {
-            menu[i].classList.remove("viewable");   
-            menu[i].style.left = window.innerWidth/2;
-        }      
-    }
-    header.addEventListener("mouseover", mainheadHandler);
-    header.addEventListener("mouseout",mainheadHandler2);
-    
-    //"run" handler
-    run.addEventListener("click", submit);
+    catch(e) {}
     
     //replaceAll function
     String.prototype.replaceAll = function(search, replacement) {
@@ -53,48 +56,38 @@ window.onload = function() {
             var report = document.createElement("report");
             report.innerHTML = arguments[i] + "<br>";
             reports.appendChild(report);
-            }
+        }
+    }
+    
+    //for making sure replacements don't collide with the actual used words
+    function encode(str) {
+        return str.split('').map(function(val) {
+            return String.fromCharCode(val.charCodeAt(0) + 13);
+        }).join('');
+    }
+    function decode(str) {
+        return str.split('').map(function(val) {
+            return String.fromCharCode(val.charCodeAt(0) - 13);
+        }).join('');
+    }
+    
+    //the function that replaces the words
+    function replace(val, obj) {
+        var val2 = val;
+        for (key in obj) {
+            val2 = val2.replaceAll(key, encode(obj[key]));
+        }
+        console.log(val2);
+        for (key in obj) {
+            val2 = val2.replaceAll(encode(obj[key]), obj[key]);
+        }
+        console.log(val2);
+        return val2;
     }
     
     //code evaluator
-    function evaluate(val, choice) {
-        
-    //replace with placeholders
-    var val2 = "";
-    val2 = val.replaceAll("var","#or");
-    val2 = val2.replaceAll("for","#ar");
-    val2 = val2.replaceAll("while","#unction");
-    val2 = val2.replaceAll("function","#hile");
-    val2 = val2.replaceAll("{","left#squarebracket");
-    val2 = val2.replaceAll("}","right#squarebracket");
-    val2 = val2.replaceAll("(","left#curlybracket");
-    val2 = val2.replaceAll(")","right#curlybracket");
-    val2 = val2.replaceAll("[","left#parenthesis");
-    val2 = val2.replaceAll("]","right#parenthesis");
-    val2 = val2.replaceAll("+","minus#sign");
-    val2 = val2.replaceAll("-","plus#sign");
-    val2 = val2.replaceAll("|","a#nd");
-    val2 = val2.replaceAll("&","o#r");    
-    val2 = val2.replaceAll("console.log","externalConsoleReport")
-    console.log(val2);
-    
-    //clear and replace placeholders
-    val2 = val2.replaceAll("#ar","var");
-    val2 = val2.replaceAll("#or","for");
-    val2 = val2.replaceAll("#hile","while");
-    val2 = val2.replaceAll("#unction","function");
-    val2 = val2.replaceAll("left#squarebracket","[");
-    val2 = val2.replaceAll("right#squarebracket","]");
-    val2 = val2.replaceAll("left#curlybracket","{");
-    val2 = val2.replaceAll("right#curlybracket","}");
-    val2 = val2.replaceAll("left#parenthesis","(");
-    val2 = val2.replaceAll("right#parenthesis",")");
-    val2 = val2.replaceAll("minus#sign","-");
-    val2 = val2.replaceAll("plus#sign","+");
-    val2 = val2.replaceAll("a#nd","&");
-    val2 = val2.replaceAll("o#r","|");
-    console.log(val2);
-        
-    eval(val2);
+    function evaluate(str) {
+        str = replace(str, words);
+        eval(str);
     }
 }
